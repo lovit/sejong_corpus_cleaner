1e01111ef95ac908ac344b5a6c059dadb2671c19
lovit <soy.lovit@gmail.com>
2018-10-08 01:27:17 +0900

    Commit load function

--- sejong_corpus_cleaner/utils.py
+++ sejong_corpus_cleaner/utils.py
@@ -1,5 +1,43 @@
 import subprocess
+from bs4 import BeautifulSoup
 
 def check_encoding(paths):
     for path in paths:
         print(subprocess.getstatusoutput("file %s" % path)[1])
+
+def load_written_text_as_sentences(filepath, encoding='utf-8', header=None):
+
+    if not header:
+        header = filepath.split('/')[-1][:-4]
+
+    with open(filepath, encoding='utf-16') as f:
+        text = f.read()
+    soup = BeautifulSoup(text, 'lxml')
+
+    sentences = soup.find_all('p')
+    sentences = [sent.text.strip() for sent in sentences]
+    sentences = [sent for sent in sentences if sent[:len(header)] == header]
+
+    def remove_header(sent):
+        return '\n'.join(eojeol.split('\t', 1)[-1] for eojeol in sent.split('\n'))
+
+    sentences = [remove_header(sent) for sent in sentences]
+
+    return sentences
+
+def load_spoken_text_as_sentences(filepath, encoding='utf-8', header=None):
+
+    if not header:
+        header = filepath.split('/')[-1][:-4]
+
+    with open(filepath, encoding='utf-16') as f:
+        text = f.read()
+    soup = BeautifulSoup(text, 'lxml')
+
+    sentences = str(soup.find('text'))
+    sentences = [sent.split('\t',1)[-1] for sent in sentences.split('\n')]
+
+    soup = BeautifulSoup('\n'.join(sentences), 'lxml')
+    sentences = [sent.text.strip() for sent in soup.find_all('s')]
+
+    return sentences
