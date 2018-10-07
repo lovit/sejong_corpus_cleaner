db65d9b96e8b8a432fe859b1fa31bf60617d0526
lovit <soy.lovit@gmail.com>
2018-10-08 04:38:24 +0900

    Commit Corpus

--- sejong_corpus_cleaner/__init__.py
+++ sejong_corpus_cleaner/__init__.py
@@ -4,4 +4,5 @@ from .utils import load_spoken_text_as_sentences
 from .utils import load_written_text_as_sentences
 from .utils import load_as_morphemes_sentences
 from .utils import load_as_eojeol_table
+from .utils import Corpus
 from .statistics import count_tags
--- sejong_corpus_cleaner/utils.py
+++ sejong_corpus_cleaner/utils.py
@@ -119,3 +119,17 @@ def _to_data_frame(counter):
         columns = 'Eojeol morphemes Count Is_compound'.split()
     )
     return df
+
+class Corpus:
+
+    def __init__(self, morpheme_sentence_path, num_sent=-1):
+        self.path = morpheme_sentence_path
+        self.num_sent = num_sent
+
+    def __iter__(self):
+        with open(self.path, encoding='utf-8') as f:
+            for i, sent in enumerate(f):
+                if self.num_sent > 0 and i > self.num_sent:
+                    break
+                morph_poss = [token.rsplit('/', 1) for token in sent.split()]
+                yield morph_poss
