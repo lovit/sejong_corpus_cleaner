d5db2aa3bef7b139fe1c815e856950c5295b60ab
lovit <soy.lovit@gmail.com>
2019-01-28 19:04:43 +0900

    Four column <eojeol, morphtag, sep xsv as Adj, sep xsv as Noun>

--- scripts/build_lr_morphtag.py
+++ scripts/build_lr_morphtag.py
@@ -1,8 +1,9 @@
 import argparse
+import re
 
 import sys
 sys.path.insert(0, '../')
-from sejong_corpus_cleaner.simplifier import eojeol_morphtags_sentence_to_lr
+from sejong_corpus_cleaner.simplifier import eojeol_morphtags_to_lr
 from sejong_corpus_cleaner.processed_data import EojeolMorphtagSentence
 
 def main():
@@ -28,6 +29,10 @@ def main():
     with open(output_path, 'w', encoding='utf-8') as f:
         create(input_path, f)
 
+def only_hangle(s):
+    pattern = re.compile('[^가-힣]+')
+    return pattern.sub('', s)
+
 def create(input_path, f):
 
     def eojeol_to_strf(l, r, l_tag, r_tag):
@@ -43,21 +48,27 @@ def create(input_path, f):
             print('\rbuilding %d sents' % i, end='', flush=True)
 
         try:
-            sent_ = eojeol_morphtags_sentence_to_lr(sent, separate_xsv=False)
-            if len(sent_) != len(sent_):
-                n_exceptions += 1
-                continue
+            for eojeol, morphtags in sent:
+                lr_unsep = eojeol_morphtags_to_lr(eojeol, morphtags, separate_xsv=False)[0]
+                lr_sep = eojeol_morphtags_to_lr(eojeol, morphtags, separate_xsv=True)
+                if len(lr_sep) == 2:
+                    eojeol = only_hangle(eojeol)
+                    lr_sep = (lr_sep[0][0], eojeol[len(lr_sep[0][0]):], lr_sep[0][2], 'Josa')
+                else:
+                    lr_sep = lr_sep[0]
 
-            sent_ = [eojeol_to_strf(l, r, l_tag, r_tag) for l, r, l_tag, r_tag in sent_]
-            for (eojeol, morphtags), lr in zip(sent, sent_):
                 morphtags_strf = ' '.join(['{}/{}'.format(m,t) for m,t in morphtags])
-                f.write('{}\t{}\t{}\n'.format(eojeol, morphtags_strf, lr))
+                lr_unsep_strf = eojeol_to_strf(*lr_unsep)
+                lr_sep_strf = eojeol_to_strf(*lr_sep)
+                f.write('{}\t{}\t{}\t{}\n'.format(eojeol, morphtags_strf, lr_unsep_strf, lr_sep_strf))
             f.write('\n')
 
         except Exception as e:
             n_exceptions += 1
             #print('\nException: sent # {}'.format(i))
             #print(e)
+            #print(lr_unsep)
+            #print(lr_sep)
             #print()
             #break
 
