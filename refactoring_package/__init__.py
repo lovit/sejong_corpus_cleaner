5ee3fc63a0603e79967b5ca0a2f93252df7dbf3d
lovit <soy.lovit@gmail.com>
2019-07-24 16:20:54 +0200

    Skeleton of to_lr

--- refactoring_package/__init__.py
+++ refactoring_package/__init__.py
@@ -1,10 +1,12 @@
 __author__ = 'lovit'
 
+from .format_checker import does_sent_have_wrong_tag
 from .loader import load_a_file
+from .lr import to_lr
 from .simple_tag import to_simple_tag
 from .simple_tag import to_simple_morphtags
 from .utils import check_encoding
 
 __all__ = [
-    'load_a_file', 'to_simple_tag', 'to_simple_morphtags', 'check_encoding'
+    'does_sent_have_wrong_tag', 'load_a_file', 'to_lr', 'to_simple_tag', 'to_simple_morphtags', 'check_encoding'
 ]
---
+++ refactoring_package/lr.py
@@ -0,0 +1,2 @@
+def to_lr(eojeol, morphtags, xsv_as_verb=False):
+    raise NotImplemented
