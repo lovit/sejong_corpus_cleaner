d93070a66a0c0821910fc9f350215c955396eb5f
lovit <soy.lovit@gmail.com>
2019-07-24 17:34:43 +0900

    Read text as XML form

--- sejong_corpus_cleaner/loader.py
+++ sejong_corpus_cleaner/loader.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import os
 
 
@@ -9,7 +10,32 @@ def is_colloquial_file(path):
     --------
     path : str
         File path
+
+    Returns
+    -------
+    flag : Booolean
+        Weather the file at path is colloquial corpus or not
     """
     path = os.path.abspath(path)
     filename = path.split(sep)[-1]
     return '_' in filename
+
+def read_txt_as_soup(path, encoding='utf-16'):
+    """
+    Argument
+    --------
+    path : str
+        File path
+
+    Returns
+    -------
+    soup : BeautifulSoup
+        XML formed document
+    """
+    try:
+        with open(path, encoding=encoding) as f:
+            text = f.read()
+            soup = BeautifulSoup(text, 'lxml')
+            return soup
+    except:
+        raise ValueError('Failed to read txt: {}'.format(path))
