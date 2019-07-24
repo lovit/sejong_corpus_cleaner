9bb020833c53f60f633f52c6eac7bf7a24fb53bd
lovit <soy.lovit@gmail.com>
2019-07-24 14:41:14 +0200

    Define __all__

--- refactoring_package/__init__.py
+++ refactoring_package/__init__.py
@@ -5,3 +5,8 @@ from .simple_tag import to_simple_tag
 from .simple_tag import to_simple_morphtags
 from .utils import check_encoding
 from .utils import select_morphtags
+
+__all__ = [
+    'extract_sentence', 'to_simple_tag', 'to_simple_morphtags',
+    'check_encoding', 'select_morphtags'
+]
