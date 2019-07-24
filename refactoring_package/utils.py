eaeb4087ec0222193606ae4bd45bbed21d02b41d
lovit <soy.lovit@gmail.com>
2019-07-24 14:39:58 +0200

    Migrate functions for handling hangle

--- refactoring_package/utils.py
+++ refactoring_package/utils.py
@@ -163,4 +163,77 @@ def unicode_character(c):
     return unicode_mapper.get(c, c)
 
 def unicode_sentence(sent):
+    """
+    Argument
+    --------
+    sent : str
+        Input sentence
+
+    Returns
+    -------
+    sent : str
+        Fix non-unicode character
+    """
     return ''.join(unicode_character(c) for c in sent)
+
+hangle_begin = 44032
+hangle_end = 55203
+chosung_base  = 588
+jungsung_base = 28
+jaum_begin = 12593
+jaum_end = 12622
+moum_begin = 12623
+moum_end = 12643
+
+chosung_list = [
+    'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ',
+    'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ',
+    'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ',
+    'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+]
+
+jungsung_list = [
+    'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ',
+    'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ',
+    'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ',
+    'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'
+]
+
+jongsung_list = [
+    ' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ',
+    'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ',
+    'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ',
+    'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ',
+    'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ',
+    'ㅌ', 'ㅍ', 'ㅎ'
+]
+
+def is_hangle(c):
+    return hangle_begin <= ord(c) <= hangle_end
+
+def is_jaum(c):
+    return jaum_begin <= ord(c) <= jaum_end
+
+def is_moum(c):
+    return moum_begin <= ord(c) <= moum_end
+
+def compose(chosung, jungsung, jongsung):
+    hangle = chr(
+        hangle_begin +
+        chosung_base * chosung_list.index(chosung) +
+        jungsung_base * jungsung_list.index(jungsung) +
+        jongsung_list.index(jongsung)
+    )
+    return hangle
+
+def decompose(c):
+    i = ord(c)
+    if (jaum_begin <= i <= jaum_end):
+        return (c, ' ', ' ')
+    if (moum_begin <= i <= moum_end):
+        return (' ', c, ' ')
+    i -= hangle_begin
+    cho  = i // chosung_base
+    jung = ( i - cho * chosung_base ) // jungsung_base
+    jong = ( i - cho * chosung_base - jung * jungsung_base )
+    return (chosung_list[cho], jungsung_list[jung], jongsung_list[jong])
