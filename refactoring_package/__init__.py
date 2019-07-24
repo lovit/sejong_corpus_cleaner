aff283408bd9d2778f325d29888eeed51a3baac0
lovit <soy.lovit@gmail.com>
2019-07-24 11:53:21 +0200

    Default tag simplifier

--- refactoring_package/__init__.py
+++ refactoring_package/__init__.py
@@ -1,4 +1,7 @@
 __author__ = 'lovit'
 
 from .loader import extract_sentences
+from .simple_tag import to_simple_tag
+from .simple_tag import to_simple_morphtags
 from .utils import check_encoding
+from .utils import select_morphtags
