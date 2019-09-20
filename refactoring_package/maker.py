9541e24b8681d891fb2ce2fa1f2740f5ee170de5
lovit <soy.lovit@gmail.com>
2019-09-20 20:35:18 +0900

    Eomi converter: R -> sejong format

--- refactoring_package/maker.py
+++ refactoring_package/maker.py
@@ -1,12 +1,56 @@
 from collections import defaultdict
-from .lr import to_lr
+from .lr import to_lr, preprocess0, preprocess1
 
 
-def make_eojeol_morphemes_table(table_file_path, data_dir=None,
-    convert_lr=False, xsv_as_verb=False, corpus_types=None):
+def make_lr_eomi_to_sejong_converter(sents, noun_xsv_as_verb):
+    """
+    Arguments
+    ---------
+    sentences : list of Sentence or Sentences
+        Iterable object consists with Sentence instance
+    noun_xsv_as_verb : Boolean
+        Option for L-R format transformation.
+        If True, it considers Noun + XSV as verb.
+        For example,
 
-    paths = prepare_data_paths(corpus_types, data_dir)
-    raise NotImplemented
+            $ "시작/NNG + 하/XSV + 다/EP" -> "시작하/Verb + 다/Eomi"
+
+        Else it consider XSV + Eomi as surfacial form of verb,
+
+            $ "시작/NNG + 하/XSV + 다/EP" -> "시작/Noun + 하다/Verb"
+
+    Returns
+    ------
+    rules : list of tuple
+        (l, sejong format morphemes, count)
+        For example,
+
+            ...
+            ((음에/Eomi, (음/ETN, 에/JKB)), 19),
+            ((겠다고/Eomi, (겠/EP, 다/EC, 고/JKQ)), 19),
+            ((으세요/Eomi, (으시/EP, 어요/EF)), 19),
+            ((었느냐고/Eomi, (었/EP, 느냐고/EC)), 19),
+            ...
+    """
+    counter = make_counter(sents, convert_lr=False, eojeol_morpheme_pair=True)
+    sorted_counter = sorted(counter.items(), key=lambda x:-x[1])
+    converter = defaultdict(int)
+    num_exceptions, count_exceptions = 0, 0
+    for (eojeol, morphtags), count in sorted_counter:
+        try:
+            eojeol, l, r, morphtags, b = to_lr(eojeol, morphtags, noun_xsv_as_verb)[0]
+        except:
+            num_exceptions += 1
+            count_exceptions += count
+            continue
+        if r is None or r.tag != 'Eomi':
+            continue
+        converter[(r, tuple(morphtags[b+1:]))] += count
+
+    perc = 100 * count_exceptions / sum(counter.values())
+    args = (len(converter), num_exceptions, '%.3f' % perc)
+    print('Create {} rules with {} ({} %) errors'.format(*args))
+    return sorted(converter.items(), key=lambda x:-x[1])
 
 def make_counter(sentences, eojeol_morpheme_pair=True, convert_lr=False,
     noun_xsv_as_verb=False, xsv_as_root=False, show_exception_cases=False):
