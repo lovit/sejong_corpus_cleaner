61a5c2fd80ba9baf84e206ca3e91a354e1329452
lovit <soy.lovit@gmail.com>
2019-09-30 20:43:19 +0900

    Commit update all scripts

---
+++ scripts/update_all.sh
@@ -0,0 +1,6 @@
+python build_corpus.py  --corpus_type type1
+python build_corpus.py  --corpus_type type2
+python build_corpus.py  --corpus_type type3
+python build_counter.py --corpus_type type1
+python build_counter.py --corpus_type type2
+python build_counter.py --corpus_type type3
